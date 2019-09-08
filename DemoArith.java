fceed32a743a517c02e387e1d756c33c6ea5e75e
Pranav Patil <patilpranavrkn@gmail.com>
2019-09-08 19:50:23 +0530

    Add files via upload

---
+++ DemoArith.java
@@ -0,0 +1,30 @@
+class DemoArith
+{
+	
+	public static void main(String args[])
+	{
+
+
+
+		System.out.println(Arithmetic.add(3,11));
+
+
+	}
+
+
+
+}
+class Arithmetic
+{
+	
+	public static int add(int a ,int b)
+	{
+		int c;
+
+		c=a+b;
+
+		return c;
+
+	}
+
+}
